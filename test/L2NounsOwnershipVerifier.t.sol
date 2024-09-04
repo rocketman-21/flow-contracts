e0838c36f6179dd05f519e5145c5f424659e1adf
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-09-04 10:55:10 -0400

    add l2nouns verifier!

---
+++ test/L2NounsOwnershipVerifier.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import "forge-std/StdJson.sol";
+
+import {StateVerifier} from "../src/state-proof/StateVerifier.sol";
+import {L2NounsOwnershipVerifier} from "../src/state-proof/L2NounsOwnershipVerifier.sol";
+import {MerkleTree} from "relic-contracts/lib/MerkleTree.sol";
+
+contract L2NounsOwnershipVerifierTest is Test {
+    using stdJson for string;
+
+    function test() public {
+        vm.createSelectFork("https://mainnet.base.org", 18997612);
+        L2NounsOwnershipVerifier verifier = new L2NounsOwnershipVerifier();
+        string memory rootPath = vm.projectRoot();
+        string memory path = string.concat(rootPath, "/test/data.json");
+        string memory json = vm.readFile(path);
+
+        StateVerifier.StateProofParameters memory params = StateVerifier.StateProofParameters({
+            beaconRoot: json.readBytes32(".beaconRoot"),
+            beaconOracleTimestamp: uint256(json.readBytes32(".beaconOracleTimestamp")),
+            executionStateRoot: json.readBytes32(".executionStateRoot"),
+            stateRootProof: abi.decode(json.parseRaw(".stateRootProof"), (bytes32[])),
+            storageProof: abi.decode(json.parseRaw(".storageProof"), (bytes[])),
+            accountProof: abi.decode(json.parseRaw(".accountProof"), (bytes[]))
+        });
+        assertTrue(verifier.isOwner(256, 0xb1a32FC9F9D8b2cf86C068Cae13108809547ef71, params));
+    }
+}
