c32b6415e68f72a223a478f9b68e902725ffdbc8
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-09-10 18:17:15 -0400

    add basic functions

---
+++ src/tcr/utils/CappedMath.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// CappedMath.sol is a modified version of Kleros' CappedMath.sol:
+// https://github.com/kleros/tcr
+//
+// CappedMath.sol source code Copyright Kleros licensed under the MIT license.
+
+pragma solidity ^0.8.27;
+
+/**
+ * @title CappedMath
+ * @dev Math operations with caps for under and overflow.
+ */
+library CappedMath {
+    uint256 private constant UINT_MAX = 2 ** 256 - 1;
+
+    /**
+     * @dev Adds two unsigned integers, returns 2^256 - 1 on overflow.
+     */
+    function addCap(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        uint256 c = _a + _b;
+        return c >= _a ? c : UINT_MAX;
+    }
+
+    /**
+     * @dev Subtracts two integers, returns 0 on underflow.
+     */
+    function subCap(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        if (_b > _a) return 0;
+        else return _a - _b;
+    }
+
+    /**
+     * @dev Multiplies two unsigned integers, returns 2^256 - 1 on overflow.
+     */
+    function mulCap(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring '_a' not being zero, but the
+        // benefit is lost if '_b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (_a == 0) return 0;
+
+        uint256 c = _a * _b;
+        return c / _a == _b ? c : UINT_MAX;
+    }
+}
