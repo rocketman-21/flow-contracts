f2529c145c23bb1ebf2a5c78cef89bc91764a92e
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-09-19 16:35:39 -0400

    make safer

--- src/library/FlowRecipients.sol
+++ src/library/FlowRecipients.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.27;
 
 import { FlowTypes } from "../storage/FlowStorageV1.sol";
 import { IFlowEvents, IFlow } from "../interfaces/IFlow.sol";
-import { ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
-import { ISuperfluidPool } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/gdav1/ISuperfluidPool.sol";
 
 library FlowRecipients {
     /**
@@ -13,7 +11,7 @@ library FlowRecipients {
      * @dev Only callable by the manager of the contract
      * @dev Emits a RecipientRemoved event if the recipient is successfully removed
      */
-    function removeRecipient(FlowTypes.Storage storage fs, bytes32 recipientId) external returns (address) {
+    function removeRecipient(FlowTypes.Storage storage fs, bytes32 recipientId) external {
         if (fs.recipients[recipientId].recipient == address(0)) revert IFlow.INVALID_RECIPIENT_ID();
         if (fs.recipients[recipientId].removed) revert IFlow.RECIPIENT_ALREADY_REMOVED();
 
@@ -24,7 +22,5 @@ library FlowRecipients {
 
         fs.recipients[recipientId].removed = true;
         fs.activeRecipientCount--;
-
-        return recipientAddress;
     }
 }
